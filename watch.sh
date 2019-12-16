198467c5ed8658f64eb7df01cf97c7cade708442
Paul Downey <psd@whatfettle.com>
2019-12-16 11:34:30 +0000

    Make watch

--- watch.sh
+++ watch.sh
@@ -1,5 +1,7 @@
 export brownfield_land_index=http://localhost:8000/brownfield-land-collection/collection/index.json
 
+make
+
 while inotifywait -e close_write ../brownfield-land-collection/collection/index.json 
 do
   make
