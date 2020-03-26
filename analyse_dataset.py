3587db0ec9babdf8f574e3f59571e0e0ce0767f1
Colm Britton <colmjude@gmail.com>
2020-03-26 12:37:49 +0000

    Summarise brownfield land dataset

---
+++ analyse_dataset.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import csv
+
+import pandas as pd
+
+dataset_url = 'https://github.com/digital-land/brownfield-land-collection/tree/master/index/dataset.csv?raw=true'
+
+
+class DatasetAnalyser():
+
+    def __init__(self):
+        super().__init__()
+        self.pd_data = self.panda_read()
+        self.json_data = json.loads(self.pd_data.to_json(orient='records'))
+
+    def panda_read(self):
+        # read local file
+        data = pd.read_csv('data/dataset.csv', sep=",")
+        return data
+
+    def number_of_records(self):
+        return len(self.json_data)
+
+    def active_records(self):
+        return [x for x in self.json_data if x['end-date'] is None]
+
+    def organisations(self):
+        orgs = [x['organisation'] for x in self.json_data]
+        return set(orgs)
+
+    # dwelling analysis
+    # currently uses min dwelling figure
+    def largest_dwelling_count(self):
+        return self.pd_data['minimum-net-dwellings'].max()
+
+    def total_dwellings(self):
+        min_dwellings = [x['minimum-net-dwellings']
+                         for x in self.json_data if x['minimum-net-dwellings'] is not None]
+        return sum(min_dwellings)
+
+    # hectare analysis
+    def largest_hectare_value(self):
+        return self.pd_data['hectare'].max()
+
+    def total_hectares(self):
+        hectares = [x['hectares']
+                    for x in self.json_data if x['hectares'] is not None]
+        return "{0:.2f}".format(sum(hectares))
+
+    def summary(self):
+        return {
+            'records': self.number_of_records(),
+            'active_records': len(self.active_records()),
+            'hectares': self.total_hectares(),
+            'dwellings': int(self.total_dwellings()),
+            'organisations': len(self.organisations())
+        }
