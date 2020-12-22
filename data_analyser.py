bb08888a7a139afd53c6085426fbc93aa07cf7b2
Colm Britton <colmjude@gmail.com>
2020-12-22 12:41:42 +0000

    Data_analyser needs to handle point field

--- data_analyser.py
+++ data_analyser.py
@@ -3,8 +3,7 @@
 from numbers import Number
 
 
-class DataAnalyser():
-
+class DataAnalyser:
     def __init__(self, data, transformed=False):
         super().__init__()
         self.data = data
@@ -19,59 +18,134 @@ class DataAnalyser():
 
     def count_pip_active(self):
         if self.transformed:
-            permission_types = [x["planning-permission-type"] for x in self.data if x['end-date'] is None]
+            permission_types = [
+                x["planning-permission-type"]
+                for x in self.data
+                if x["end-date"] is None
+            ]
         else:
-            permission_types = [x["PermissionType"] for x in self.data if x['EndDate'] is None]
+            permission_types = [
+                x["PermissionType"] for x in self.data if x["EndDate"] is None
+            ]
         return permission_types.count("permission in principle")
 
     def countHasLatLng(self):
         if self.transformed:
-            return len([(row['latitude'], row['longitude']) for row in self.data if row['longitude'] and row['latitude']])
-        return len([(row['GeoY'], row['GeoX']) for row in self.data if row['GeoX'] and row['GeoY']])
+            return len([row["point"] for row in self.data if row["point"] is not None])
+        return len(
+            [
+                (row["GeoY"], row["GeoX"])
+                for row in self.data
+                if row["GeoX"] and row["GeoY"]
+            ]
+        )
 
     def end_date_count(self):
         if self.transformed:
-            return len([x['end-date'] for x in self.data if x['end-date'] is not None])
-        return len([x['EndDate'] for x in self.data if x['EndDate'] is not None])
+            return len([x["end-date"] for x in self.data if x["end-date"] is not None])
+        return len([x["EndDate"] for x in self.data if x["EndDate"] is not None])
 
     def row_count(self):
         return len(self.data)
 
     def org_uris(self):
         if self.transformed:
-            return [row['organisation'] for row in self.data]
-        return [row['OrganisationURI'] for row in self.data]
+            return [row["organisation"] for row in self.data]
+        return [row["OrganisationURI"] for row in self.data]
 
     def sum_max_dwellings(self):
         if self.transformed:
-            return sum([x['maximum-net-dwellings'] for x in self.data if isinstance(x['maximum-net-dwellings'], Number)])
-        return sum([x['NetDwellingsRangeTo'] for x in self.data if isinstance(x['NetDwellingsRangeTo'], Number)])
+            return sum(
+                [
+                    x["maximum-net-dwellings"]
+                    for x in self.data
+                    if isinstance(x["maximum-net-dwellings"], Number)
+                ]
+            )
+        return sum(
+            [
+                x["NetDwellingsRangeTo"]
+                for x in self.data
+                if isinstance(x["NetDwellingsRangeTo"], Number)
+            ]
+        )
 
     def sum_max_dwellings_active(self):
         if self.transformed:
-            return sum([x['maximum-net-dwellings'] for x in self.data if isinstance(x['maximum-net-dwellings'], Number) and x['end-date'] is None])
-        return sum([x['NetDwellingsRangeTo'] for x in self.data if isinstance(x['NetDwellingsRangeTo'], Number) and x['EndDate'] is None])
+            return sum(
+                [
+                    x["maximum-net-dwellings"]
+                    for x in self.data
+                    if isinstance(x["maximum-net-dwellings"], Number)
+                    and x["end-date"] is None
+                ]
+            )
+        return sum(
+            [
+                x["NetDwellingsRangeTo"]
+                for x in self.data
+                if isinstance(x["NetDwellingsRangeTo"], Number) and x["EndDate"] is None
+            ]
+        )
 
     def sum_min_dwellings(self):
         if self.transformed:
-            return sum([x['minimum-net-dwellings'] for x in self.data if isinstance(x['minimum-net-dwellings'], Number)])
-        return sum([x['NetDwellingsRangeFrom'] for x in self.data if isinstance(x['NetDwellingsRangeFrom'], Number)])
+            return sum(
+                [
+                    x["minimum-net-dwellings"]
+                    for x in self.data
+                    if isinstance(x["minimum-net-dwellings"], Number)
+                ]
+            )
+        return sum(
+            [
+                x["NetDwellingsRangeFrom"]
+                for x in self.data
+                if isinstance(x["NetDwellingsRangeFrom"], Number)
+            ]
+        )
 
     def sum_min_dwellings_active(self):
         if self.transformed:
-            return sum([x['minimum-net-dwellings'] for x in self.data if isinstance(x['minimum-net-dwellings'], Number) and x['end-date'] is None])
-        return sum([x['NetDwellingsRangeFrom'] for x in self.data if isinstance(x['NetDwellingsRangeFrom'], Number) and x['EndDate'] is None])
+            return sum(
+                [
+                    x["minimum-net-dwellings"]
+                    for x in self.data
+                    if isinstance(x["minimum-net-dwellings"], Number)
+                    and x["end-date"] is None
+                ]
+            )
+        return sum(
+            [
+                x["NetDwellingsRangeFrom"]
+                for x in self.data
+                if isinstance(x["NetDwellingsRangeFrom"], Number)
+                and x["EndDate"] is None
+            ]
+        )
 
     def total_hectares(self):
         if self.transformed:
-            return sum([x['hectares'] for x in self.data if isinstance(x['hectares'], Number)])
-        return sum([x['Hectares'] for x in self.data if isinstance(x['Hectares'], Number)])
+            return sum(
+                [x["hectares"] for x in self.data if isinstance(x["hectares"], Number)]
+            )
+        return sum(
+            [x["Hectares"] for x in self.data if isinstance(x["Hectares"], Number)]
+        )
 
     def total_hectares_active(self):
         if self.transformed:
-            h = [x['hectares'] for x in self.data if isinstance(x['hectares'], Number) and x['end-date'] is None]
+            h = [
+                x["hectares"]
+                for x in self.data
+                if isinstance(x["hectares"], Number) and x["end-date"] is None
+            ]
         else:
-            h = [x['Hectares'] for x in self.data if isinstance(x['Hectares'], Number) and x['EndDate'] is None]
+            h = [
+                x["Hectares"]
+                for x in self.data
+                if isinstance(x["Hectares"], Number) and x["EndDate"] is None
+            ]
         return sum(h)
 
     def summary(self):
@@ -87,5 +161,5 @@ class DataAnalyser():
             "min_dwellings_active": self.sum_min_dwellings_active(),
             "permission_in_principle": self.count_pip(),
             "permission_in_principle_active": self.count_pip_active(),
-            "unique_organisation_uris": len(set(self.org_uris()))
+            "unique_organisation_uris": len(set(self.org_uris())),
         }
